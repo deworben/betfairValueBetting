c064808d668497be8af98440d3da10536c44c037
deworben <bdeworsop@gmail.com>
2020-03-24 15:56:02 +1100

    adding black formatting

--- 0.readingFiles/parseAllFiles.py
+++ 0.readingFiles/parseAllFiles.py
@@ -1,13 +1,12 @@
-from parseOneFile import *
 import os
-import matplotlib.pyplot as plt
-import pandas as pd
+from parseOneFile import readFile
 import numpy as np
+
 
 # run this script once per data set to create a saved list where you can perform analysis
 dataTimeframe = "oct_data"
 currentDir = os.path.dirname(os.path.abspath(__file__))
-path_to_folder = os.path.join(   currentDir ,  ".." ,  "data", dataTimeframe)
+path_to_folder = os.path.join(currentDir, "..", "data", dataTimeframe)
 
 i = 0
 
@@ -17,23 +16,24 @@ listOfLists = []
 
 
 for file in os.listdir(path_to_folder):
-    if not file.endswith('.bz2'):
+    if not file.endswith(".bz2"):
         (timeLists, priceLists) = readFile(path_to_folder + "\\" + file)
         if timeLists:
             for index, times in enumerate(timeLists):
                 # adds individual time-price array paris intio seperate array
                 listOfLists.append(
-                    [timeLists[index].copy(), priceLists[index].copy()])
+                    [timeLists[index].copy(), priceLists[index].copy()]
+                )
 
     i += 1
     # Print the number of files read to the screen
-    if(i % 500 == 0):
+    if i % 500 == 0:
         print(i)
 
 
 try:
     os.remove(outputDump)
-except:
+finally:
     pass
 # Save all the data that's been parsed
 np.save(outputDump, listOfLists)
